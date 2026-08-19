06bbc7f1cb3c5336218e368a96ee182b506923d7
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:15:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,7 +15,5 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /usr/bin/taradino
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
